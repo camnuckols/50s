535f2776fbe8026662de832876877ded3984c66c
Cameron Nuckols <camnuckols@gmail.com>
2017-05-20 15:02:27 -0600

    Added a fun feature

--- src/App.js
+++ src/App.js
@@ -17,28 +17,28 @@ class App extends Component {
       page: 1
     }
     this.changePage = this.changePage.bind( this );
+    this.turnOnReminders = this.turnOnReminders.bind( this );
   }
   changePage( num ) {
     this.setState( { page: num } );
   }
+  turnOnReminders() {
+
+  }
   render() {
     return (
-      // <Router>
         <div className="App-container">
           <Navbar />
-          {/*<Route exact path="/" component={ Home } />
-          <Route exact path="/analytics" component={ Analytics } />
-          <Route exact path="/leaderboard" component={ Leaderboard } />*/}
-          { 
-            
-          <div className="Home-container" className="Home-google-form" style={{display: this.state.page === 1 ? "block" : "none"}}>
+          { <div className="Home-container" className="Home-google-form" style={{display: this.state.page === 1 ? "block" : "none"}}>
               <iframe className="Home-google-form" src="https://docs.google.com/forms/d/e/1FAIpQLSfVl3pzu4Unwde43IvYFxY3Uu7YluNjAokpBAVWyx8uK0v_ZA/viewform?embedded=true">Loading...</iframe>
           </div> }
           { <iframe className="Analytics-iframe" style={{display: this.state.page === 2 ? "block" : "none"}} src="https://docs.google.com/spreadsheets/d/19vPgVhWyjyGW5-m-wbOkdigUntPnAe3w3w_Vkp3adJI/pubhtml?gid=407975155&amp;single=true&amp;widget=true&amp;headers=false"></iframe> }
           { <iframe className="Analytics-iframe" style={{display: this.state.page === 3 ? "block" : "none"}} src="https://docs.google.com/spreadsheets/d/19vPgVhWyjyGW5-m-wbOkdigUntPnAe3w3w_Vkp3adJI/pubhtml?gid=1911393012&amp;single=true&amp;widget=true&amp;headers=false"></iframe> }
+          { <div className="Home-google-form get-motivation" style={{display: this.state.page === 4 ? "block" : "none"}}>
+              <a href="sms:/18018210113/&body=I need a little motivation today."><button>Get Motivation</button></a>
+          </div> }
           <Footer changePage={ this.changePage } active={ this.state.page } />
         </div>
-      //</Router>
     );
   }
 }
--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -3,6 +3,7 @@ import './Footer.css';
 import Fifty from 'react-icons/lib/fa/500px';
 import Chart from 'react-icons/lib/fa/area-chart';
 import Trophy from 'react-icons/lib/fa/trophy';
+import Motivation from 'react-icons/lib/md/wb-incandescent';
 import { NavLink } from 'react-router-dom';
 
 class Footer extends Component {
@@ -16,21 +17,18 @@ class Footer extends Component {
   render() {
     return (
     <div className="Footer-container">
-        {/*<NavLink to="/" exact={ true } activeClassName="Footer-active">*/}
-            <button onClick={ () => this.props.changePage( 1 ) } className="Footer-icon-container">
-                <Fifty size={ 20 } id="Footer-fifty-icon" className={ this.props.active === 1 ? this.state.active : this.state.notActive } />
-            </button>
-        {/*</NavLink>*/}
-        {/*<NavLink to="/analytics" activeClassName="Footer-active">*/}
-            <button onClick={ () => this.props.changePage( 2 ) } className="Footer-icon-container">
-                <Chart size={ 20 } id="Footer-chart-icon" className={ this.props.active === 2 ? this.state.active : this.state.notActive } />
-            </button>
-        {/*</NavLink>*/}
-        {/*<NavLink to="/leaderboard" activeClassName="Footer-active">*/}
-            <button onClick={ () => this.props.changePage( 3 ) } className="Footer-icon-container">
-                <Trophy size={ 20 } id="Footer-trophy-icon" className={ this.props.active === 3 ? this.state.active : this.state.notActive } />
-            </button>
-        {/*</NavLink>*/}
+        <button onClick={ () => this.props.changePage( 1 ) } className="Footer-icon-container">
+            <Fifty size={ 20 } id="Footer-fifty-icon" className={ this.props.active === 1 ? this.state.active : this.state.notActive } />
+        </button>
+        <button onClick={ () => this.props.changePage( 2 ) } className="Footer-icon-container">
+            <Chart size={ 20 } id="Footer-chart-icon" className={ this.props.active === 2 ? this.state.active : this.state.notActive } />
+        </button>
+        <button onClick={ () => this.props.changePage( 3 ) } className="Footer-icon-container">
+            <Trophy size={ 20 } id="Footer-trophy-icon" className={ this.props.active === 3 ? this.state.active : this.state.notActive } />
+        </button>
+        <button onClick={ () => this.props.changePage( 4 ) } className="Footer-icon-container">
+            <Motivation size={ 20 } id="Footer-trophy-icon" className={ this.props.active === 4 ? this.state.active : this.state.notActive } />
+        </button>
     </div>
     );
   }

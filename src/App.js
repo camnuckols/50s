3f009677b727a30a933d70097ce7a88abceeb9f3
Cameron Nuckols <camnuckols@gmail.com>
2017-07-19 21:27:53 -0600

    Added fities icons

--- src/App.js
+++ src/App.js
@@ -1,29 +1,27 @@
 import React, { Component } from 'react';
 import './App.css';
-import {
-  BrowserRouter as Router,
-  Route
-} from 'react-router-dom';
 import Navbar from './components/navbar/Navbar';
 import Footer from './components/footer/Footer';
 import Home from './components/home/Home';
 import Analytics from './components/analytics/Analytics';
 import Leaderboard from './components/leaderboard/Leaderboard';
+import Countdown from 'react-countdown-clock';
+import moment from 'moment';
 
 class App extends Component {
   constructor( props ) {
     super( props );
+    var d = new Date();
+    let msSinceMidnight = d.getTime() - d.setHours(0,0,0,0);
+    let midnight = ( 86400000 - msSinceMidnight ) / 1000;
     this.state = {
-      page: 1
+      page: 1,
+      midnight: midnight
     }
     this.changePage = this.changePage.bind( this );
-    this.turnOnReminders = this.turnOnReminders.bind( this );
   }
   changePage( num ) {
     this.setState( { page: num } );
-  }
-  turnOnReminders() {
-
   }
   render() {
     return (
@@ -34,8 +32,12 @@ class App extends Component {
           </div> }
           { <iframe className="Analytics-iframe" style={{display: this.state.page === 2 ? "block" : "none"}} src="https://docs.google.com/spreadsheets/d/19vPgVhWyjyGW5-m-wbOkdigUntPnAe3w3w_Vkp3adJI/pubhtml?gid=407975155&amp;single=true&amp;widget=true&amp;headers=false"></iframe> }
           { <iframe className="Analytics-iframe" style={{display: this.state.page === 3 ? "block" : "none"}} src="https://docs.google.com/spreadsheets/d/19vPgVhWyjyGW5-m-wbOkdigUntPnAe3w3w_Vkp3adJI/pubhtml?gid=1911393012&amp;single=true&amp;widget=true&amp;headers=false"></iframe> }
-          { <div className="Home-google-form get-motivation" style={{display: this.state.page === 4 ? "block" : "none"}}>
-              <a href="sms:/18018210113/&body=I need a little motivation today."><button>Get Motivation</button></a>
+          { <div className="Home-google-form" style={{display: this.state.page === 4 ? "block" : "none"}}>
+              <a href="sms:/18018210113/&body=Give me some motivation!"><button className="get-motivation">Get Motivated</button></a>
+              <Countdown seconds={ this.state.midnight }
+                     color="#1B98E0"
+                     alpha={ 0.9 }
+                     size={ 300 } />
           </div> }
           <Footer changePage={ this.changePage } active={ this.state.page } />
         </div>
